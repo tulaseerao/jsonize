07556d194526167f34ad229d71ae2f179a0868c6
tulaseerao <tulasi.chintha@gmail.com>
2014-01-06 21:27:57 -0500

    Added intial jsonize

--- jsonize.rb
+++ jsonize.rb
@@ -3,26 +3,6 @@
 require 'jsonize/deserialize'
 require 'jsonize/serialize'
 
-# Public: Extends your model with methods to allow serialization and deserialization between it and a hash. This works
-# hand-in-hand with the JSON API as seen below.
-#
-# Examples
-#
-#   class Person
-#     include Jsonize
-#     field :name
-#   end
-#   class Car
-#     include Jsonize
-#     field :make
-#     collection :passengers, type: Person, source: 'riders'
-#   end
-#   car_json = '{"make":"Honda","riders":[{"name":"Tony"}]}'
-#   car = Car.deserialize(JSON.parse(car_json))
-#   car.make       #=> "Honda"
-#   car.passengers #=> [#<Person @name=Tony>]
-#   car.serialize  #=> {"make" => "Honda", "riders" => [{"name" => "Tony"}]}
-#   JSON.generate(car.serialize) #=> '{"make":"Honda","riders":[{"name":"Tony"}]}'
 module Jsonize
   include Jsonize::Deserialize
   include Jsonize::Serialize
